77e6d848f86b0c397b45155f675b405ce723fdbf
LeeSangHa <38284431+StrangeLee@users.noreply.github.com>
2019-12-19 08:59:18 +0900

    Initial commit

---
+++ src/hash/CallNumberList.java
@@ -0,0 +1,26 @@
+package hash;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class CallNumberList {
+
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+        HashMap<String, String> phoneMap = new HashMap<>();
+        HashSet<String> phoneHash = new HashSet<>();
+
+        for (String phone :
+                phone_book) {
+            //phoneMap.put(phone, phoneMap.containsKey());
+            phoneHash.add(phone);
+        }
+
+        return answer = true;
+    }
+
+    public static void main(String[] args) {
+        String [] phone_book  = {"123", "123789", "456"};
+        System.out.println(new CallNumberList().solution(phone_book));
+    }
+}
---
+++ src/linked_list/LinkedListExample.java
@@ -0,0 +1,14 @@
+package linked_list;
+
+import java.util.LinkedList;
+
+public class LinkedListExample {
+
+    public LinkedListExample() {
+        LinkedList<String> linkedList = new LinkedList<>();
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
