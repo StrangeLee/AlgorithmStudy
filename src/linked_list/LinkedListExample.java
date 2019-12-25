539d0422b073e89ee105f493e0e345be0c9d2cad
LeeSangHa <38284431+StrangeLee@users.noreply.github.com>
2019-12-25 20:46:05 +0900

    주석 추가 및 수정

--- src/linked_list/LinkedListExample.java
+++ src/linked_list/LinkedListExample.java
@@ -6,9 +6,26 @@ public class LinkedListExample {
 
     public LinkedListExample() {
         LinkedList<String> linkedList = new LinkedList<>();
+        var items = "lee kim choi jun go han".split(" ");
+
+        for (int i = 0; i < items.length; i++) {
+            linkedList.add(items[i]);
+        }
+
+        System.out.println("using foreach");
+        for (String str:
+             linkedList) {
+            System.out.println(str);
+        }
+
+        System.out.println(linkedList.contains("lee"));
+        System.out.println("using fori");
+        for (int i = 0; i < linkedList.size(); i++) {
+            System.out.println(linkedList.get(i));
+        }
     }
 
     public static void main(String[] args) {
-
+        new LinkedListExample();
     }
 }
--- src/stack/StackExample.java
+++ src/stack/StackExample.java
@@ -10,10 +10,12 @@ public class StackExample {
             stack.push(i + 1); // stack 에 데이터 삽입
             System.out.println(stack.peek()); // 최근에 추가한 데이터 참조(read 만)
         }
-        stack.pop(); // 최근에 추가한 데이터 삭제
-        System.out.println(stack.peek());
-        System.out.println(stack.search(1));
-        System.out.println(stack.empty()); // false 면 값이 있음, true 면 없음.
+        System.out.println(stack.pop()); // 최근에 추가한 데이터 삭제
+        System.out.println(stack.lastElement());
+        System.out.println("last added is " + stack.peek());
+        int item = 1;
+        System.out.println("searching " + item + " result " + stack.search(item)); // 4 반환
+        System.out.println("stack is empty ? " + (stack.empty() ? "yes" : "no")); // false 면 값이 있음, true 면 없음.
     }
 
     public static void main(String[] args) {
